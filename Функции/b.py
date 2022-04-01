942a6b1b053d64f2be088a140836b2d889417538
Marlenqyzy <78716452+Marlenqyzy@users.noreply.github.com>
2022-04-01 21:59:17 +0600

    Add files via upload

---
+++ Функции/b.py
@@ -0,0 +1,6 @@
+def power(a, b):
+    print(a ** b)
+
+a, b = input().split()
+
+power(float(a), int(b))
---
+++ Функции/c.py
@@ -0,0 +1,6 @@
+def xor(a, b):
+    print(a^b)
+
+a, b = input().split()
+
+xor(int(a), int(b))
